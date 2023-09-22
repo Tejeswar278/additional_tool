891ac13da4d8cd5c0be6e51c6487d1064d5b499c
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-09-22 12:17:25 +0530

    updated patches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import bgimg from "./assets/background.png"
+import example from "./assets/example.png"
 import "./App.css"
  
 const Secondpage = () => {
@@ -13,8 +14,9 @@ const Secondpage = () => {
                         Digital Interface
                     </div>
                     <p>
-                      
+                      Choreograph every element of your shower experience with built-in Wi-Fi, Bluetooth and voice activated technology when paired with google home or amazon ro alexa pluz use the DFC@Home app to craft up to twelve shower preset with spa options remotely warm your steam shower and customize the interface
                     </p>
+                    <img className='btmimg' src={example} alt='example/>'/>
                 </div>
             </div>
         </>
